e75d87c2f223eaddf2235601f107c19830be036b
Ali Arda İsenkul <ardaisenkul@hotmail.com>
2022-01-06 01:50:07 +0300

    first commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Ali Arda İsenkul on 6.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
